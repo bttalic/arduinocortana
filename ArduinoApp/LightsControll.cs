6fb7155cdd94cd034b23d89ee235e1b0159cd7a2
bttalic <b.ttalic@gmail.com>
2015-08-29 13:51:30 +0200

    Initial commit

---
+++ ArduinoApp/LightsControll.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Maker.RemoteWiring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoApp
+{
+    class LightsControll
+    {
+
+        private static LightsControll controll;
+
+        public static LightsControll SetLightControll(string bulb, string action)
+        {
+            controll = new LightsControll(bulb, action);
+
+            return controll;
+        }
+
+        public static LightsControll GetLightControll()
+        {
+            if (controll == null)
+            {
+                controll = new LightsControll();
+            }
+
+            return controll;
+        }
+
+        private LightsControll()
+        { }
+
+        private LightsControll(string bulb, string action)
+        {
+            SetValues(bulb, action);
+        }
+
+        public byte Pin { get; set; }
+
+        public PinState State { get; private set; }
+
+
+        private void SetValues(string bulb, string action)
+        {
+            switch (bulb.ToLower())
+            {
+                case "red":
+                    Pin = 13;
+                    break;
+                case "green":
+                    Pin = 12;
+                    break;
+                case "blue":
+                    Pin = 11;
+                    break;
+            }
+
+            switch (action.ToLower())
+            {
+                case "on":
+                    State = PinState.HIGH;
+                    break;
+                case "off":
+                    State = PinState.LOW;
+                    break;
+            }
+        }
+    }
+}
---
+++ ArduinoApp/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Maker.Serial;
+using Microsoft.Maker.RemoteWiring;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ArduinoApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RemoteDevice arduino;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            UsbSerial connection =new UsbSerial("VID_2341", "PID_0043");
+            arduino = new RemoteDevice(connection);
+            //just start typing this and you can autocomplete using tab
+            connection.ConnectionEstablished += Connection_ConnectionEstablished;
+            arduino.DeviceReady += TurnOnLight;
+            connection.begin(57600, SerialConfig.SERIAL_8N1);
+        }
+
+        private void Connection_ConnectionEstablished()
+        {
+            //just enabling the buttons
+            OnButton.IsEnabled = true;
+            OffButton.IsEnabled = true;
+           
+        }
+
+        private void TurnOnLight()
+        {
+            LightsControll controll = LightsControll.GetLightControll();
+            arduino.digitalWrite(controll.Pin, controll.State);
+        }
+
+        private void OnButton_Click(object sender, RoutedEventArgs e)
+        {
+            arduino.digitalWrite(13, PinState.HIGH);
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            arduino.digitalWrite(13, PinState.LOW);
+        }
+    }
+}
